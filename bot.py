4a62f05b4b95e9761aac6dbaf4cac61b5957b0aa
ynovitskiy <novitskyyan@gmail.com>
2023-03-23 12:11:41 +0300

    New file cities.py has been added. The whole structure of the bot.py and database.py were changed. At this version, sqlite3 is used.

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from cities import Cities
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -5,11 +7,11 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 from database import Database
 
-
 TOKEN = "5841939580:AAH2nh8VePTxpvnuseUhOlrmRVWW5PyZeHk"
-FILENAME = "database"
-FILENAME_LIKES = "liked"
-CITIES = ["Москва", "Новгород", "Самара", "Сызрань", "Саратов", "Санкт-Петербург", ]
+cities_obj = Cities("other/russia.json")
+CITIES = cities_obj.get_cities()
+
+db = Database("user.db")
 
 
 # FUNCTION
@@ -19,11 +21,12 @@ def correct_name(name):
     return False
 
 
-
 # BUTTONS
 FORM_BTN = KeyboardButton("Заполнить анкету")
 EDIT_FORM_BTN = KeyboardButton("Редактировать анкету")
-SWIPE_FORM_BTN = KeyboardButton("Смотреть анкеты")
+SWIPE_FORM_BTN = KeyboardButton("Познакомиться")
+MBTI_BTN = KeyboardButton("Узнать MBTI")
+TAGS_BTN = KeyboardButton("Указать теги")
 LIKES_BTN = KeyboardButton("Лайки")
 STOP_BTN = KeyboardButton("Скрыть анкету")
 LIKE_BTN = KeyboardButton("😍")
@@ -39,7 +42,7 @@ ACTIVE_PROFILE_BTN = KeyboardButton("Активировать")
 FORM_KB = ReplyKeyboardMarkup(resize_keyboard=True)
 FORM_KB.add(FORM_BTN)
 MENU_KB = ReplyKeyboardMarkup(resize_keyboard=True)
-MENU_KB.add(EDIT_FORM_BTN, SWIPE_FORM_BTN, LIKES_BTN, STOP_BTN)
+MENU_KB.add(EDIT_FORM_BTN, SWIPE_FORM_BTN, MBTI_BTN, TAGS_BTN, STOP_BTN)
 CHECK_USERS = ReplyKeyboardMarkup(resize_keyboard=True)
 CHECK_USERS.add(LIKE_BTN, SKIP_BTN)
 EDIT_PROFILE_KB = ReplyKeyboardMarkup(resize_keyboard=True)
@@ -56,163 +59,121 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start_(message: types.Message):
-    users = Database.get_dict(FILENAME)
-    user_id = str(message.from_user.id)
+    user_id = message.from_user.id
     username = message.from_user.username
-    users[user_id] = {"state": "start", "active": "False",
-                      "username": username, "name": "no", "city": "no", "age": "no", "likes": []}
-    Database.write(users, FILENAME)
+    db.add_user_state(user_id, "start", "False")
+    db.add_user(user_id, username)
     await message.reply(f"Приветствуем, {message.from_user.username}!\nВ данном боте вы "
                         f"можете познакомиться", reply_markup=FORM_KB)
 
+
 @dp.message_handler(commands=['menu'])
 async def menu_(message: types.Message):
-    users = Database.get_dict(FILENAME)
-    user_id = str(message.from_user.id)
-    users[user_id]["state"] = "wait"
-    Database.write(users, FILENAME)
+    user_id = message.from_user.id
+    db.replace_state(user_id, "wait")
     await message.reply(f"Вернулись в главное меню", reply_markup=MENU_KB)
 
 
 @dp.message_handler()
 async def info(message: types.Message):
-    user_id = str(message.from_user.id)
+    user_id = message.from_user.id
     global last_user_id
-    users = Database.get_dict(FILENAME)
-    if message.text == "Заполнить анкету" and users[user_id]["state"] == "start":
-        users[user_id]["state"] = "name"
-        Database.write(users, FILENAME)
+
+    # Input file
+    if message.text == "Заполнить анкету" and db.get_state(user_id) == "start":
+        db.replace_state(user_id, "name")
         await message.reply("Введите имя: ")
-    elif users[user_id]["state"] == "name":
+    elif db.get_state(user_id) == "name":
         if correct_name(message.text):
-            users[user_id]["state"] = "city"
-            users[user_id]["name"] = message.text
-            Database.write(users, FILENAME)
+            db.replace_state(user_id, "city")
+            db.replace_name(user_id, message.text)
             await message.reply("Ваше имя принято!\nВведите теперь название города")
         else:
             await message.reply("Имя должно быть меньше 10 символов и начинаться с заглавной буквы")
-    elif users[user_id]["state"] == "city":
+    elif db.get_state(user_id) == "city":
         if message.text in CITIES:
-            users[user_id]["state"] = "age"
-            users[user_id]["city"] = message.text
-            Database.write(users, FILENAME)
+            db.replace_state(user_id, "age")
+            db.replace_city(user_id, message.text);
             await message.reply("Ваш город принят!\nВведите теперь свой возраст")
         else:
             await message.reply(f"Город не обнаружен.\nДоступные города:\n{', '.join(CITIES)}")
-    elif users[user_id]["state"] == "age":
+    elif db.get_state(user_id) == "age":
         if message.text.isdigit() and 18 <= int(message.text) <= 80:
-            users[user_id]["state"] = "description"
-            users[user_id]["age"] = message.text
-            users[user_id]["active"] = "True"
-            Database.write(users, FILENAME)
+            db.replace_state(user_id, "description")
+            db.replace_age(user_id, message.text);
             await message.reply("Введите пару слов о себе")
-    elif users[user_id]["state"] == "description":
-        users[user_id]["state"] = "wait"
-        Database.save_desc_to_file(user_id, message.text)
-        Database.write(users, FILENAME)
-        await message.reply("Анкета заполнена!", reply_markup=MENU_KB)
-    elif users[user_id]["state"] == "wait" and message.text == "Смотреть анкеты":
-        users[user_id]["state"] = "check_profiles"
-        Database.write(users, FILENAME)
-        user_info = Database.random_profile_list(FILENAME, user_id)
-        last_user_id = user_info[0]
-        await message.reply(f"{user_info[1]}\n{user_info[2]}\n{user_info[3]}\n{user_info[4]}", reply_markup=CHECK_USERS)
-    elif users[user_id]["state"] == "check_profiles" and message.text == "➡":
-        Database.write(users, FILENAME)
-        user_info = Database.random_profile_list(FILENAME, user_id)
-        last_user_id = user_info[0]
-        await message.reply(f"{user_info[1]}\n{user_info[2]}\n{user_info[3]}", reply_markup=CHECK_USERS)
-    # Edit Profile
-    elif message.text == "Редактировать анкету" and users[user_id]["state"] == "wait":
-        users[user_id]["state"] = "edit"
-        Database.write(users, FILENAME)
+    elif db.get_state(user_id) == "description":
+        db.replace_state(user_id, "wait")
+        db.replace_active(user_id, "True")
+        db.replace_description(user_id, message.text)
+        profile_list = db.get_my_profile(user_id)
+        await message.reply(
+            f"\nВаша анкета:\n{profile_list[2]}\n{profile_list[3]}\n{profile_list[4]}\n{profile_list[5]}\n{profile_list[6]}\n",
+            reply_markup=MENU_KB)
+
+    # Edit profile
+    elif message.text == "Редактировать анкету" and db.get_state(user_id) == "wait":
+        db.replace_state(user_id, "edit")
         await message.reply(f"Выберите, что хотите изменить:  ", reply_markup=EDIT_PROFILE_KB)
-    elif message.text == "Имя" and users[user_id]["state"] == "edit":
-        users[user_id]["state"] = "edit_name"
-        Database.write(users, FILENAME)
+    elif message.text == "Имя" and db.get_state(user_id) == "edit":
+        db.replace_state(user_id, "edit_name")
         await message.reply("Введите новое имя: ")
-    elif users[user_id]["state"] == "edit_name":
+    elif db.get_state(user_id) == "edit_name":
         if correct_name(message.text):
-            users[user_id]["state"] = "wait"
-            users[user_id]["name"] = message.text
-            Database.write(users, FILENAME)
-            profile_list = Database.get_my_profile(FILENAME, user_id)
+            db.replace_state(user_id, "wait")
+            db.replace_name(user_id, message.text)
+            profile_list = db.get_my_profile(user_id)
             await message.reply("Ваше имя изменено!\nНовая анкета")
             await message.reply(
-                f"\n{profile_list[0]}\n{profile_list[1]}\n{profile_list[2]}\n{Database.get_desc_user(user_id)}",
+                f"\n{profile_list[2]}\n{profile_list[3]}\n{profile_list[4]}\n{profile_list[5]}\n{profile_list[6]}\n",
                 reply_markup=MENU_KB)
         else:
             await message.reply("Имя должно быть меньше 10 символов и начинаться с заглавной буквы")
-    elif message.text == "Город" and users[user_id]["state"] == "edit":
-        users[user_id]["state"] = "edit_city"
-        Database.write(users, FILENAME)
+    elif message.text == "Город" and db.get_state(user_id) == "edit":
+        db.replace_state(user_id, "edit_city")
         await message.reply("Введите новый город: ")
-    elif users[user_id]["state"] == "edit_city":
-        users[user_id]["state"] = "wait"
-        users[user_id]["city"] = message.text
-        Database.write(users, FILENAME)
-        profile_list = Database.get_my_profile(FILENAME, user_id)
+    elif db.get_state(user_id) == "edit_city" and message.text in CITIES:
+        db.replace_state(user_id, "wait")
+        db.replace_city(user_id, message.text)
+        profile_list = db.get_my_profile(user_id)
         await message.reply("Ваш город изменен!")
-        await message.reply(f"\n{profile_list[0]}\n{profile_list[1]}\n{profile_list[2]}\n{Database.get_desc_user(user_id)}", reply_markup=MENU_KB)
-    elif message.text == "Возраст" and users[user_id]["state"] == "edit":
-        users[user_id]["state"] = "edit_age"
-        Database.write(users, FILENAME)
+        await message.reply(
+            f"\n{profile_list[2]}\n{profile_list[3]}\n{profile_list[4]}\n{profile_list[5]}\n{profile_list[6]}\n",
+            reply_markup=MENU_KB)
+    elif message.text == "Возраст" and db.get_state(user_id) == "edit":
+        db.replace_state(user_id, "edit_age")
         await message.reply("Введите новый возраст: ")
-    elif users[user_id]["state"] == "edit_age":
+    elif db.get_state(user_id) == "edit_age":
         if message.text.isdigit() and 18 <= int(message.text) <= 80:
-            users[user_id]["state"] = "wait"
-            users[user_id]["age"] = message.text
-            Database.write(users, FILENAME)
-            profile_list = Database.get_my_profile(FILENAME, user_id)
+            db.replace_state(user_id, "wait")
+            db.replace_age(user_id, message.text)
+            profile_list = db.get_my_profile(user_id)
             await message.reply("Ваш возраст изменен!")
             await message.reply(
-                f"\n{profile_list[0]}\n{profile_list[1]}\n{profile_list[2]}\n{Database.get_desc_user(user_id)}",
+                f"\n{profile_list[2]}\n{profile_list[3]}\n{profile_list[4]}\n{profile_list[5]}\n{profile_list[6]}\n",
                 reply_markup=MENU_KB)
-    elif message.text == "Описание" and users[user_id]["state"] == "edit":
-        users[user_id]["state"] = "edit_desc"
-        Database.write(users, FILENAME)
-        await message.reply(f"Прошлое описание: {Database.get_desc_user(user_id)}\nВведите новое описание:")
-    elif users[user_id]["state"] == "edit_desc":
-        users[user_id]["state"] = "wait"
-        Database.save_desc_to_file(user_id, message.text)
-        Database.write(users, FILENAME)
-        profile_list = Database.get_my_profile(FILENAME, user_id)
-        await message.reply(f"\n{profile_list[0]}\n{profile_list[1]}\n{profile_list[2]}\n{Database.get_desc_user(user_id)}", reply_markup=MENU_KB)
+    elif message.text == "Описание" and db.get_state(user_id) == "edit":
+        db.replace_state(user_id, "edit_desc")
+        profile_list = db.get_my_profile(user_id)
+        await message.reply(f"Прошлое описание: {profile_list[5]}\nВведите новое описание:")
+    elif db.get_state(user_id) == "edit_desc":
+        db.replace_state(user_id, "wait")
+        db.replace_description(user_id, message.text)
+        profile_list = db.get_my_profile(user_id)
+        await message.reply(
+            f"\n{profile_list[2]}\n{profile_list[3]}\n{profile_list[4]}\n{profile_list[5]}\n{profile_list[6]}\n",
+            reply_markup=MENU_KB)
 
-    # Check Profiles
-    elif users[user_id]["state"] == "check_profiles" and message.text == "😍":
-        if last_user_id not in users[user_id]["likes"]:
-            users[user_id]["likes"].append(last_user_id)
-        Database.write(users, FILENAME)
-        user_info = Database.random_profile_list(FILENAME, user_id)
-        last_user_id = user_info[0]
-        await message.reply(f"{user_info[1]}\n{user_info[2]}\n{user_info[3]}", reply_markup=CHECK_USERS)
-    elif users[user_id]["state"] == "wait" and message.text == "Лайки":
-        users = Database.get_dict(FILENAME)
-        for liked_id in users[user_id]["likes"]:
-            for id in users.keys():
-                if liked_id == id:
-                    if user_id in users[id]["likes"] and users[id]["active"] == "True":
-                        user_info = Database.get_profile_by_id(users, id)
-                        await message.reply(f"@{user_info[0]}\n{user_info[1]}\n{user_info[2]}\n"
-                                            f"{user_info[3]}")
     # freeze and active profile
-    elif users[user_id]["state"] == "wait" and message.text == "Скрыть анкету":
-        users[user_id]["active"] = "False"
-        Database.write(users, FILENAME)
+    elif db.get_state(user_id) == "wait" and message.text == "Скрыть анкету":
+        db.replace_active(user_id, "False")
         await message.reply(f"Ваша анкета скрыта. Вы не можете смотреть других участников и поставленные лайки.\n"
                             f"Другие пользователи Вас не видят.\n"
                             f"Ваши данные сохранены, Вы всегда можете к нам вернуться.",
                             reply_markup=ACTIVE_KB)
-    elif users[user_id]["state"] == "wait" and message.text == "Активировать":
-        users[user_id]["active"] = "True"
-        Database.write(users, FILENAME)
+    elif db.get_state(user_id) == "wait" and message.text == "Активировать":
+        db.replace_active(user_id, "True")
         await message.reply(f"Ваша анкета снова видна в поиске!", reply_markup=MENU_KB)
-
-
-
-
-
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,81 +1,103 @@
-from random import randint, choice
+import sqlite3
 
 
 class Database:
-    @staticmethod
-    def find_user_by_id(id, filename):
-        file = open(filename + ".txt", "r", encoding="UTF-8")
-        file.seek(0)
-        for line in file.readlines():
-            user_id = line.split()[0]
-            if user_id == id:
-                return True
-        return False
+    def __init__(self, filename):
+        self.conn = sqlite3.connect(filename)
+        self.cur = self.conn.cursor()
 
-    @staticmethod
-    def write(users, filename):
-        file = open(filename + ".txt", "w", encoding="UTF-8")
-        for user, info in users.items():
-            file.write(
-                f"{user} {info['state']} {info['active']} {info['username']} {info['name']} {info['city']} {info['age']}"
-                f" {' '.join(info['likes'])}\n")
+        # main table
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS users(
+           user_id INT PRIMARY KEY,
+           username TEXT NULL,
+           name_ TEXT NULL,
+           city TEXT NULL,
+           age TEXT NULL,
+           description TEXT NULL,
+           mbti TEXT NULL);
+        """)
 
-    @staticmethod
-    def get_dict(filename):
-        file = open(filename + ".txt", "r", encoding="UTF-8")
-        file.seek(0)
-        users = {}
-        for line in file.readlines():
-            line_list = line.split()
-            if len(line_list) != 0:
-                id = line_list[0]
-                state = line_list[1]
-                active = line_list[2]
-                username = line_list[3]
-                name = line_list[4]
-                city = line_list[5]
-                age = line_list[6]
-                likes = line_list[7:]
-                users[id] = {"state": state, "active": active, "username": username,
-                             "name": name, "city": city, "age": age, "likes": likes}
-        return users
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS users_state(
+                   user_id INT PRIMARY KEY,
+                   state_ TEXT,
+                   active TEXT);
+                """)
 
-    @staticmethod
-    def random_profile_list(filename, id):
-        users = Database.get_dict(filename)
-        users.pop(id)
-        del_users = []
-        for user in users.keys():
-            if users[user]["active"] == "False":
-                del_users.append(user)
-        for deleted in del_users:
-            users.pop(deleted)
-        user_id = choice(list(users.keys()))
-        info = users[user_id]
-        user_info = [user_id, info["name"], info["city"], info["age"], Database.get_desc_user(id)]
-        return user_info
+        self.conn.commit()
 
-    @staticmethod
-    def get_my_profile(filename, id):
-        file = open(filename + ".txt", "r", encoding="UTF-8")
-        file.seek(0)
-        for line in file.readlines():
-            l = line.split()
-            if l[0] == id:
-                return [l[4], l[5], l[6], Database.get_desc_user(id)]
+    def add_user(self, id_user, username):
+        sql = """
+                INSERT INTO users (user_id, username)
+                VALUES (?, ?)
+                """
 
-    @staticmethod
-    def get_profile_by_id(users, id):
-        for user_id in users.keys():
-            if user_id == id:
-                return [users[user_id]["username"], users[user_id]["name"], users[user_id]["city"],
-                        users[user_id]["age"], Database.get_desc_user(id)]
+        self.cur.execute(sql, (id_user, username))
+        self.conn.commit()
 
-    @staticmethod
-    def save_desc_to_file(id, desc):
-        with open("desc/" + id + ".txt", "w", encoding="UTF-8") as file:
-            file.write(desc)
+    def add_user_state(self, id_user, s, a):
+        sql = """
+            INSERT INTO users_state (user_id, state_, active)
+            VALUES (?, ?, ?)
+            """
 
-    @staticmethod
-    def get_desc_user(id):
-        return open("desc/" + id + ".txt", "r", encoding="UTF-8").read()
+        self.cur.execute(sql, (id_user, s, a))
+        self.conn.commit()
+
+    def replace_state(self, id_user, s):
+        sql = """UPDATE users_state SET state_ = ? WHERE user_id = ?"""
+
+        self.cur.execute(sql, (s, id_user))
+
+    def replace_name(self, id_user, name):
+        sql = """UPDATE users SET name_ = ? WHERE user_id = ?"""
+
+        self.cur.execute(sql, (name, id_user))
+        self.conn.commit()
+
+    def replace_city(self, id_user, city):
+        sql = """UPDATE users SET city = ? WHERE user_id = ?"""
+
+        self.cur.execute(sql, (city, id_user))
+        self.conn.commit()
+
+    def replace_age(self, id_user, age):
+        sql = """UPDATE users SET age = ? WHERE user_id = ?"""
+
+        self.cur.execute(sql, (age, id_user))
+        self.conn.commit()
+
+    def replace_description(self, id_user, description):
+        sql = """UPDATE users SET description = ? WHERE user_id = ?"""
+
+        self.cur.execute(sql, (description, id_user))
+        self.conn.commit()
+
+    def replace_active(self, id_user, act):
+        sql = """UPDATE users_state SET active = ? WHERE user_id = ?"""
+
+        self.cur.execute(sql, (act, id_user))
+        self.conn.commit()
+
+    def count_row_users(self, id_users):
+        sql = """ SELECT COUNT(*) FROM users"""
+        self.cur.execute(sql)
+        return self.cur.fetchone()[0]
+
+    def get_state(self, id_user):
+        sql = """ SELECT state_ from users_state where user_id = ?"""
+        self.cur.execute(sql, (id_user,))
+        return self.cur.fetchone()[0]
+
+    def get_random_profile(self, id_user):
+        sql = """ SELECT * from users where user_id != ?"""
+
+        self.cur.execute(sql, (id_user,))
+        info = self.cur.fetchall()
+        return info
+
+    def get_my_profile(self, id_user):
+        sql = """ SELECT * from users where user_id == ?"""
+
+        self.cur.execute(sql, (id_user,))
+        info = self.cur.fetchone()
+        return info
